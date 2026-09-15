a11b99b074e774593abfd844d69be6ab3b0b9478
decio2022 <113923924+decio2022@users.noreply.github.com>
2026-09-15 16:30:46 -0300

    Minor fixes

--- lngi.js
+++ lngi.js
@@ -280,7 +280,6 @@ function update() {
     last_tick = now;
     var deltaRealTime = now - lastRealTime;
     virtualElapsed += deltaRealTime*(pause%2)
-    console.log(deltaRealTime)
     lastRealTime = now;
     var simulatedTime = st + virtualElapsed + timeOffset;
     var u = num_time(simulatedTime);
