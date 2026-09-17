44e59471fd45c01c58b6fb39a781ba43157d8a62
decio2022 <113923924+decio2022@users.noreply.github.com>
2026-09-17 13:45:04 -0300

    i said test too many times, so yeah another test

--- ui.js
+++ ui.js
@@ -216,7 +216,7 @@ function saveAllSettings() {
 function saveMisc() {
     const misc = {
         //time based events
-        time: player_time
+        time: simulatedTime
     }
     localStorage.setItem("lngi_app_misc", JSON.stringify(misc))
 }
