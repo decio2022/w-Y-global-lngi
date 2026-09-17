3997ad6fa12a637110395173c0b39797c760e379
decio2022 <113923924+decio2022@users.noreply.github.com>
2026-09-17 13:23:51 -0300

    more tests

--- lngi.js
+++ lngi.js
@@ -283,6 +283,7 @@ function update() {
     lastRealTime = now;
     var simulatedTime = st + virtualElapsed + timeOffset;
     var u = num_time(simulatedTime);
+    player_time = virtualElapsed
 
     document.getElementById("main_lngi_Content").innerHTML = `<i>${u[2]}</i>`
     document.getElementById("main_lngi_bar").innerHTML = `${u[0]} to next ordinal (${u[1]} left)`
--- ui.js
+++ ui.js
@@ -216,7 +216,7 @@ function saveAllSettings() {
 function saveMisc() {
     const misc = {
         //time based events
-        time: virtualElapsed
+        time: player_time
     }
     localStorage.setItem("lngi_app_misc", JSON.stringify(misc))
 }
